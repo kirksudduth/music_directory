86d2193a5b5cfe0e3fdfc429b4f89fd898f437bb
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-30 13:43:23 -0500

    forgot to be doing these commits and i did it on master.. oops

---
+++ Music.session.sql
@@ -0,0 +1,78 @@
+SELECT SongId, Title, ReleaseDate
+FROM Song;
+
+SELECT *
+FROM Genre;
+
+INSERT INTO
+Artist
+VALUES
+    (null, "Madlib", 1997);
+
+INSERT INTO
+Album
+    (AlbumId,
+    Title,
+    ReleaseDate,
+    AlbumLength,
+    Label,
+    ArtistId,
+    GenreId
+    )
+SELECT null,
+    "Bandana",
+    "08/25/2019",
+    2847,
+    "Fuzzy Ear",
+    ArtistId,
+    GenreId
+FROM Artist, Genre
+WHERE Artist.ArtistName = "Madlib" and Genre.Label = "Rap";
+
+INSERT INTO
+Album
+    (AlbumId,
+    Title,
+    ReleaseDate,
+    AlbumLength,
+    Label,
+    ArtistId,
+    GenreId
+    )
+SELECT null,
+    "Bad Neighbor Instrumentals",
+    "02/16/2015",
+    2587,
+    "Fuzzy Ear",
+    ArtistId,
+    GenreId
+FROM Artist, Genre
+WHERE Artist.ArtistName = "Madlib" and Genre.Label = "Rap";
+
+
+INSERT INTO
+Song
+    (SongId, Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+SELECT null, "Peroxide", 530, Album.ReleaseDate, Genre.GenreId, Artist.ArtistId, Album.AlbumId
+FROM Album, Genre, Artist
+WHERE Album.Title LIKE "%Bad%" and Genre.Label = "Rap" and Artist.ArtistName LIKE "%Madl%" and Album.Title LIKE "%Bad Neigh%";
+
+INSERT INTO
+Song
+    (SongId, Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+SELECT null, "Knock Knock", 287, Album.ReleaseDate, Genre.GenreId, Artist.ArtistId, Album.AlbumId
+FROM Album, Genre, Artist
+WHERE Album.Title LIKE "%Bad%" and Genre.Label = "Rap" and Artist.ArtistName LIKE "%Madl%" and Album.Title LIKE "%Bad Neigh%";
+
+INSERT INTO
+Song
+    (SongId, Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+SELECT null, "Crime Pays", 269, Album.ReleaseDate, Genre.GenreId, Artist.ArtistId, Album.AlbumId
+FROM Album, Genre, Artist
+WHERE Album.Title LIKE "%Banda%" and Genre.Label = "Rap" and Artist.ArtistName LIKE "%Madl%" AND Album.Title LIKE "%Banda%";
+
+SELECT Song.Title as "Song Title", Album.Title as "Album Title", Artist.ArtistName as "Artist Name"
+FROM Song
+    JOIN Album ON Song.AlbumId = Album.AlbumId
+    JOIN Artist ON Song.ArtistId = Artist.ArtistId
+WHERE Artist.ArtistName = "Madlib";
